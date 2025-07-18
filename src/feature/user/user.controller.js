ac3723315df5b4aee2d5e238ff9e6f9982d33da6
codebygurpreet <geekygurpreet4@gmail.com>
2025-07-18 22:57:38 +0530

    feat(jwt): add token generation and route protection middleware

--- src/feature/user/user.controller.js
+++ src/feature/user/user.controller.js
@@ -1,24 +1,39 @@
 import UserModel from "./user.model.js";
+import jwt from "jsonwebtoken";
 
-export default class UserController{
+export default class UserController {
 
-    signUp(req,res){
-        const {name,email,password} = req.body;
-        const newUser = UserModel.signUp(name,email,password);
-        if (newUser){
+    signUp(req, res) {
+        const { name, email, password } = req.body;
+        const newUser = UserModel.signUp(name, email, password);
+        if (newUser) {
             res.status(201).json({ message: 'User registered successfully', user: newUser });
-        }else{
+        } else {
             res.status(400).send("User already exists")
         }
     }
-    
-    signIn(req,res){
-        const {email,password} = req.body;
-        const user = UserModel.signIn(email,password);
-        if (user) {
-            res.status(200).json({ message: 'user logged In', user: user });
-        }else{
+
+    signIn(req, res) {
+        const { email, password } = req.body;
+        const result = UserModel.signIn(email, password);
+        if (!result) {
             res.status(400).send("Invalid Credentials")
+        } else {
+            // 1. create token
+            const token = jwt.sign(
+                {
+                    userID: result.id,
+                    email: result.email
+                },
+                "x8T0Gv2zmfOgOAa2tCcvLOaaOPuI2roE"
+                ,
+                {
+                    expiresIn: "1h"
+                }
+            )
+
+            // 2. send token
+            return res.status(200).send(token);
         }
     }
 
---
+++ src/middleware/jwt.middleware.js
@@ -0,0 +1,28 @@
+// import required packages
+import jwt from "jsonwebtoken";
+
+const jwtAuth = (req,res) => {
+
+    // 1. read the token
+    const token = req.headers['authorization']; 
+
+    // 2. if no token , send error
+    if(!token){
+        return res.status(401).send("Unauthorized")
+    }
+    
+    // 3. check if token is valid
+    try{
+        const payload =  jwt.verify(token,
+            "x8T0Gv2zmfOgOAa2tCcvLOaaOPuI2roE"
+        )
+        // req.userID = payload.userID;
+    }catch(err){
+        // 4. return err
+        console.log(err)
+        return res.status(401).send("Unauthorized")
+    }
+
+    // 5. call next middleware
+    next();
+}
